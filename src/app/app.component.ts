558cd4c5c498e40e68d3a998b95b8c6c60b34ee0
Renata Secco <renata.secco@gmail.com>
2020-04-24 00:12:34 -0300

    chore:componentesok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'internet-banking-avanade';
-}
+  constructor(public dialog: MatDialog) {
+
+  }
+  openDialog() {
+    const dialogRef = this.dialog.open(AppComponent, {
+      height: '350px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  }
+
