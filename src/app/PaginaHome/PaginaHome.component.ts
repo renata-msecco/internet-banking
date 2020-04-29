0600cb62aaed70fb9b60f32075bcca2156439bb3
jbockhorny <jbockhorny@gmail.com>
2020-04-29 20:47:05 -0300

    funcionalidades: Botão sair, inicio da transferencia, confirmação e correção do saldo

--- src/app/PaginaHome/PaginaHome.component.ts
+++ src/app/PaginaHome/PaginaHome.component.ts
@@ -29,7 +29,7 @@ export class PaginaHomeComponent implements OnInit {
   classNome = 'textNome';
   textAux = 'textAux';
   valorSaldo = 'valorSaldo';
-  saldoInConta = 0;
+  saldoInConta = null;
 
   constructor(
     private router: Router,
@@ -38,30 +38,31 @@ export class PaginaHomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUser();
-    // this.user = new User();
-    // this.user.Id = Number(window.localStorage.getItem('cliente_id'));
-    // this.user.Nome = window.localStorage.getItem('cliente_nome');
-    // this.user.Agencia = window.localStorage.getItem('cliente_agencia');
-    // this.user.Conta = window.localStorage.getItem('cliente_conta');
+    this.user = new User();
+    this.user.Id = Number(window.localStorage.getItem('cliente_id'));
+    this.user.Nome = window.localStorage.getItem('cliente_nome');
+    this.user.Agencia = window.localStorage.getItem('cliente_agencia');
+    this.user.Conta = window.localStorage.getItem('cliente_conta');
   }
 
   getUser() {
     const userId = window.localStorage.getItem('cliente_id');
     this.http.get(`https://retro-bank-api.azurewebsites.net/clientes/${userId}`).subscribe((cliente: User) => {
        this.user = cliente;
-       this.saldoInConta = cliente.SaldoI.toFixed(2);
+       this.saldoInConta = cliente.SaldoI.toFixed(2).toString();
     });
-  }
-
-
-  clicou() {
-    console.log(this.user.SaldoI);
   }
   showTransfer() {
     this.router.navigate(['transferencia']);
   }
   showExtra() {
     this.router.navigate(['extrato']);
+  }
+
+  sair(){
+    console.log('sair');
+    this.router.navigate(['login']);
+    localStorage.clear();
   }
 }
   // clientes.http.get('https://retro-bank-api.azurewebsites.net').subscribe((clientes: User[]) => {
