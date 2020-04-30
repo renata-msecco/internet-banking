ae85d003f69f589f4b817c64c892e8c4440bcda2
AndrezaAmorim <amrimandreza@gmail.com>
2020-04-30 13:53:08 -0300

    Estilização da pagina Extrato

--- src/app/PaginaExtrato/PaginaExtrato.component.ts
+++ src/app/PaginaExtrato/PaginaExtrato.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-pagina-extrato',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaExtratoComponent implements OnInit {
 
-  constructor() { }
+  user: User = null;
+  classFooter = 'buttonFooter';
+  nameButtonFooter = 'nameButtonFooter';
+  nameBtnFooter = 'nameBtnFooter';
+  titleButtonFotter = 'Sair';
+  titleBtnFotter = 'Home';
+  submit = 'submit';
+  Extrato = 'Extrato';
+  classNome = 'textExtrato';
+  transfData = '16/06/2020';
+  textData = 'Data:';
+  textValor = 'R$';
+  valor = '100,00';
+  descricao = 'Valor transferido bla bla bla';
+
+  constructor(
+    private router: Router,
+
+  ) { }
 
   ngOnInit(): void {
+    this.user = new User();
+    this.user.Id = Number(window.localStorage.getItem('cliente_id'));
+    this.user.Nome = window.localStorage.getItem('cliente_nome');
+    this.user.Agencia = window.localStorage.getItem('cliente_agencia');
+    this.user.Conta = window.localStorage.getItem('cliente_conta');
+  }
+
+  Home() {
+    this.router.navigate(['home']);
   }
 
 }
